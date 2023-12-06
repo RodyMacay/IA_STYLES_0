cffbb521910c08776e29a78528f8e53dc29427f9
Rody Macay <rodymacay123@gmail.com>
2023-12-06 16:18:19 -0500

    requirements.txt

--- Config/urls.py
+++ Config/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('', include('Apps.Usuario.urls')),
     path('',include('Apps.PublicacionDprendas.urls')),
     path('',include('Apps.ReconocimientosDPrecio.urls'))
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
